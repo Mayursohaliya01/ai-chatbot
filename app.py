2a8b795dfaaf2bc54f7adf45d0ef4a137aba6164
Mayursohaliya01 <133564807+Mayursohaliya01@users.noreply.github.com>
2026-03-09 21:43:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+st.title("SOHALIYA'S Chatbot 🤖")
+st.caption("Built with Groq + Streamlit 🚀")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    st.chat_message(message["role"]).write(message["content"])
+
+prompt = st.chat_input("Ask something...")
+
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=st.session_state.messages
+    )
+
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
